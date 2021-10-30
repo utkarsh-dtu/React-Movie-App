bc41c5779d6e68ceb458c1820ae04a15cda2ecc5
Utkarsh Ramachandra <utkarshr.dce@gmail.com>
2021-10-30 19:01:28 +0530

    Adding reducers and store

--- src/index.js
+++ src/index.js
@@ -2,6 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
+import { createStore } from 'redux';
+
+import movies from './reducers'
+
+// the movie inside is the reducer
+
+const store = createStore(movies); // get the initial state
+console.log('store', store);
+console.log('STATE', store.getState());
+
 
 ReactDOM.render(
   <React.StrictMode>
